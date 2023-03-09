37508ab1b5ff0f80b34e9e5a12f277123b749008
Fabio-Alessandro <120126746+Fabio-Alessandro@users.noreply.github.com>
2023-03-08 18:59:24 -0600

    Revert "User edit/admin routes"

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require ("express");
-const router = express.Router ();
-const users = require ("./users");
-const admin = require ("./admin")
-
-router.use ("/user", users);
-router.use ("/user/admin", admin);
-
-module.exports = router;
