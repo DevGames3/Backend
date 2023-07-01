c44b71784345505087d76584c4d62a22b8e419c8
alex <durandaj027@gmail.com>
2023-07-01 15:51:47 -0400

    deploying 1

--- db/index.js
+++ db/index.js
@@ -2,8 +2,8 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const DB_NAME = process.env.DB_NAME;
-const DB_USER = JSON.parse(process.env.DB_USER);
-const DB_PASSWORD = JSON.parse(process.env.DB_PASSWORD);
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_HOST = process.env.DB_HOST;
 const DB_DIALECT = process.env.DB_DIALECT;
 
@@ -14,3 +14,9 @@ const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 });
 
 module.exports = db;
+
+alex_emeb_user;
+
+alex_emeb;
+
+v0c5PETBQ1Q8F8jzzc1oYDrPR6Ycv0wr;
